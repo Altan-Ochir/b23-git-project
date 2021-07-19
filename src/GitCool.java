d5ffeb4516487ee57733f3903ca55d41dedebf28
Altan-Ochir <altanochir0130@yahoo.com>
2021-07-19 15:50:56 -0500

    add new things

--- src/GitCool.java
+++ src/GitCool.java
@@ -3,8 +3,11 @@ public class GitCool {
     // B23 IS LEARNING GIT
     // B23 IS SPECIEAL NUMBER
     // B23 IS MAKING SOME SENSE
-    public static void main(String[] args) {
 
-    }
-
+    // we have added our github account
+    // we were able to successfulyy shared project under our github account
+    // it combines the 3 actions below
+    // create remote respository under your account
+    // create the link between local
+    //
 }
